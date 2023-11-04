c0e31f74577a7854a45ea2be7d47f0ffe85a046b
SkylerPark <012tjdals012@gmail.com>
2023-11-05 01:24:05 +0900

    test: test

--- vpc/main.tf
+++ vpc/main.tf
@@ -49,7 +49,7 @@ resource "aws_internet_gateway" "this" {
 }
 
 locals {
-  availability_zones = distinct([for subnet in local.subnets : subnet.availability_zone])
+  availability_zones = distinct([for subnet in var.subnets : subnet.availability_zone])
   nat_gateway_count  = var.single_nat_gateway ? var.one_nat_gateway_per_az ? length(local.availability_zones) : 1 : 0
 }
 
