cfc5ef828d727b6902af6aa1ffac96c461aa995a
SkylerPark <012tjdals012@gmail.com>
2023-11-04 17:33:41 +0900

    test: vpc update
    g

--- vpc/main.tf
+++ vpc/main.tf
@@ -22,21 +22,27 @@ resource "aws_vpc" "this" {
 locals {
   subnets = flatten([
     for k, v in var.subnets : [
-      for cidr in v.cidr_blocks : "${k}/${cidr}"
+      for cidr in v.cidr_blocks : {
+        name                    = try(v.name, format("%s-%s-subnet-%s", var.name, k, var.availability_zones[index(keys(local.subnets), each.key) % length(var.availability_zones)]))
+        key                     = "${k}/${cidr}"
+        cidr_block              = cidr
+        availability_zone       = try(v.availability_zone, var.availability_zones[index(keys(local.subnets), each.key) % length(var.availability_zones)])
+        map_public_ip_on_launch = try(v.map_public_ip_on_launch, false)
+      }
     ]
   ])
 }
 
 resource "aws_subnet" "this" {
-  for_each = { for subnet in local.subnets : subnet => subnet }
+  for_each = { for subnet in local.subnets : subnet.key => subnet }
 
   vpc_id                  = var.vpc_id != "" ? aws_vpc.this[0].id : var.vpc_id
   cidr_block              = each.value.cidr_block
-  availability_zone       = try(each.value.availability_zone, var.availability_zones[index(keys(local.subnets), each.key) % length(var.subnets)])
-  map_public_ip_on_launch = try(each.value.map_public_ip_on_launch, false)
+  availability_zone       = each.value.availability_zone
+  map_public_ip_on_launch = each.value.map_public_ip_on_launch
 
   tags = merge(
-    { "Name" = try(each.value.name, format("%s-subnet-%s", var.name, var.availability_zones[index(keys(local.subnets), each.key) % length(var.subnets)])) },
+    { "Name" = each.value.name },
     try(each.value.tags, {}),
     var.subnet_tags,
   )
