6d9772b466e22563bc959d55bebfd2504eda5246
SkylerPark <012tjdals012@gmail.com>
2023-11-05 01:22:51 +0900

    test: vpc update

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,5 +1,7 @@
 locals {
   vpc_name = var.vpc_name != "" ? var.vpc_name : "${var.name}-vpc-${data.aws_region.current.name}"
+  vpc_id   = var.vpc_id != "" ? aws_vpc.this[0].id : var.vpc_id
+  igw_name = var.igw_name != "" ? var.igw_name : "${var.name}-igw-${data.aws_region.current.name}"
 }
 
 data "aws_region" "current" {}
@@ -22,14 +24,90 @@ resource "aws_vpc" "this" {
 resource "aws_subnet" "this" {
   for_each = { for subnet in var.subnets : "${subnet.name}/${subnet.availability_zone}/${subnet.cidr_block}" => subnet }
 
-  vpc_id                  = var.vpc_id != "" ? aws_vpc.this[0].id : var.vpc_id
+  vpc_id                  = local.vpc_id
   cidr_block              = each.value.cidr_block
   availability_zone       = each.value.availability_zone
   map_public_ip_on_launch = try(each.value.map_public_ip_on_launch, false)
 
   tags = merge(
-    { "Name" = "${each.value.name}-${each.value.availability_zone}" },
+    { "Name" = "${each.value.name}-${each.value.tier}-subnet-${each.value.availability_zone}", "Tier" = "${each.value.tier}" },
     try(each.value.tags, {}),
     var.subnet_tags,
   )
+}
+
+resource "aws_internet_gateway" "this" {
+  count = var.create_igw ? 1 : 0
+
+  vpc_id = local.vpc_id
+
+  tags = merge(
+    { "Name" = local.igw_name },
+    var.tags,
+    var.igw_tags,
+  )
+}
+
+locals {
+  availability_zones = distinct([for subnet in local.subnets : subnet.availability_zone])
+  nat_gateway_count  = var.single_nat_gateway ? var.one_nat_gateway_per_az ? length(local.availability_zones) : 1 : 0
+}
+
+resource "aws_eip" "this" {
+  count = var.create_vpc && var.create_nat_gw ? local.nat_gateway_count : 0
+
+  domain = "vpc"
+
+  tags = merge(
+    {
+      "Name" = format(
+        "${var.name}-natgw-%s",
+        element(local.availability_zone, var.single_nat_gateway ? 0 : count.index),
+      )
+    },
+    var.tags,
+    var.nat_eip_tags,
+  )
+
+  depends_on = [aws_internet_gateway.this]
+}
+
+data "aws_subnets" "public" {
+  count = var.create_nat_gw ? 1 : 0
+  filter {
+    name   = "vpc-id"
+    values = [local.vpc_id]
+  }
+
+  tags = {
+    Tier = "public"
+  }
+
+  depends_on = [aws_subnet.this]
+}
+
+resource "aws_nat_gateway" "this" {
+  count = local.create_vpc && var.create_nat_gw ? local.nat_gateway_count : 0
+
+  allocation_id = element(
+    aws_eip.this,
+    var.single_nat_gateway ? 0 : count.index,
+  )
+  subnet_id = element(
+    data.aws_subnets.public[0].ids,
+    var.single_nat_gateway ? 0 : count.index,
+  )
+
+  tags = merge(
+    {
+      "Name" = format(
+        "${var.name}-natgw-%s",
+        element(var.availability_zone, var.single_nat_gateway ? 0 : count.index),
+      )
+    },
+    var.tags,
+    var.nat_gw_tags,
+  )
+
+  depends_on = [aws_internet_gateway.this]
 }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,4 +1,26 @@
 ################################################################################
+# Common
+################################################################################
+
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
+}
+
+variable "vpc_id" {
+  description = "The VPC ID."
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+################################################################################
 # VPC
 ################################################################################
 
@@ -9,13 +31,7 @@ variable "create_vpc" {
 }
 
 variable "vpc_name" {
-  description = "VPC Name to be used on all the resources as identifier"
-  type        = string
-  default     = ""
-}
-
-variable "name" {
-  description = "Name to be used on all the resources as identifier"
+  description = "VPC Name"
   type        = string
   default     = ""
 }
@@ -50,21 +66,9 @@ variable "vpc_tags" {
   default     = {}
 }
 
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  type        = map(string)
-  default     = {}
-}
-
 ################################################################################
 # Subnet
 ################################################################################
-
-variable "vpc_id" {
-  description = "The VPC ID."
-  type        = string
-  default     = ""
-}
 
 variable "subnets" {
   description = "Subnet List"
@@ -78,3 +82,58 @@ variable "subnet_tags" {
   default     = {}
 }
 
+################################################################################
+# Internet Gateway
+################################################################################
+
+variable "igw_name" {
+  description = "IGW Name"
+  type        = string
+  default     = ""
+}
+
+variable "create_igw" {
+  description = "Controls if an Internet Gateway is created for public subnets and the related routes that connect them"
+  type        = bool
+  default     = false
+}
+
+variable "igw_tags" {
+  description = "Additional tags for the internet gateway"
+  type        = map(string)
+  default     = {}
+}
+
+################################################################################
+# NAT Gateway
+################################################################################
+
+variable "create_nat_gw" {
+  description = "Should be true if you want to provision NAT Gateways for each of your private networks"
+  type        = bool
+  default     = false
+}
+
+variable "single_nat_gateway" {
+  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
+  type        = bool
+  default     = false
+}
+
+variable "one_nat_gateway_per_az" {
+  description = "Should be true if you want only one NAT Gateway per availability zone. Requires `var.azs` to be set, and the number of `public_subnets` created to be greater than or equal to the number of availability zones specified in `var.azs`"
+  type        = bool
+  default     = false
+}
+
+variable "nat_gw_tags" {
+  description = "Additional tags for the NAT gateways"
+  type        = map(string)
+  default     = {}
+}
+
+variable "nat_eip_tags" {
+  description = "Additional tags for the NAT EIP"
+  type        = map(string)
+  default     = {}
+}
