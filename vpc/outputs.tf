0eb037e7ca8c8634da4d04cb0ff271eec43f48be
SkylerPark <012tjdals012@gmail.com>
2023-11-06 19:29:53 +0900

    test: output test

---
+++ vpc/outputs.tf
@@ -0,0 +1,41 @@
+################################################################################
+# VPC
+################################################################################
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = try(aws_vpc.this[0].id, null)
+}
+
+output "vpc_arn" {
+  description = "The ARN of the VPC"
+  value       = try(aws_vpc.this[0].arn, null)
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = try(aws_vpc.this[0].cidr_block, null)
+}
+
+################################################################################
+# Internet Gateway
+################################################################################
+
+output "igw_id" {
+  description = "The ID of the Internet Gateway"
+  value       = try(aws_internet_gateway.this[0].id, null)
+}
+
+output "igw_arn" {
+  description = "The ARN of the Internet Gateway"
+  value       = try(aws_internet_gateway.this[0].arn, null)
+}
+
+################################################################################
+# Internet Gateway
+################################################################################
+
+output "nat_ids" {
+  description = "List of allocation ID of Elastic IPs created for AWS NAT Gateway"
+  value       = [for eip in aws_eip.this : eip.id]
+}
