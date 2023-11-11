50ea8c88596f74ef7245e8aef5d35a713cec84fe
SkylerPark <012tjdals012@gmail.com>
2023-11-11 21:18:38 +0900

    test: test

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -129,7 +129,7 @@ locals {
 data "aws_partition" "current" {}
 
 data "aws_iam_policy_document" "assume_role_policy" {
-  count = var.create && var.create_iam_instance_profile ? 1 : 0
+  count = var.create_iam_instance_profile ? 1 : 0
 
   statement {
     sid     = "EC2AssumeRole"
@@ -143,7 +143,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
 }
 
 resource "aws_iam_role" "this" {
-  count = var.create && var.create_iam_instance_profile ? 1 : 0
+  count = var.create_iam_instance_profile ? 1 : 0
 
   name        = var.iam_role_use_name_prefix ? null : local.iam_role_name
   name_prefix = var.iam_role_use_name_prefix ? "${local.iam_role_name}-" : null
@@ -158,14 +158,14 @@ resource "aws_iam_role" "this" {
 }
 
 resource "aws_iam_role_policy_attachment" "this" {
-  for_each = { for k, v in var.iam_role_policies : k => v if var.create && var.create_iam_instance_profile }
+  for_each = { for k, v in var.iam_role_policies : k => v if var.create_iam_instance_profile }
 
   policy_arn = each.value
   role       = aws_iam_role.this[0].name
 }
 
 resource "aws_iam_instance_profile" "this" {
-  count = var.create && var.create_iam_instance_profile ? 1 : 0
+  count = var.create_iam_instance_profile ? 1 : 0
 
   role = aws_iam_role.this[0].name
 
