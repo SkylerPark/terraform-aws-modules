4c1fecffdb6738f8b30c92ce697e4aeffead3c5e
SkylerPark <012tjdals012@gmail.com>
2023-11-14 00:19:00 +0900

    feat: kms update

--- eks/main.tf
+++ eks/main.tf
@@ -1,3 +1,7 @@
+################################################################################
+# Cluster
+################################################################################
+
 locals {
   cluster_name = var.cluster_name != null ? var.cluster_name : replace("${var.name}-cluster-${var.version}", ".", "-")
 }
@@ -9,10 +13,89 @@ resource "aws_eks_cluster" "this" {
   enabled_cluster_log_types = var.cluster_enabled_log_types
 
   vpc_config {
-    security_group_ids      = var.cluster_additional_security_group_ids
+    security_group_ids      = compact(distinct(concat(var.cluster_additional_security_group_ids, [local.cluster_security_group_id])))
     subnet_ids              = coalescelist(var.control_plane_subnet_ids, var.subnet_ids)
     endpoint_private_access = var.cluster_endpoint_private_access
     endpoint_public_access  = var.cluster_endpoint_public_access
     public_access_cidrs     = var.cluster_endpoint_public_access_cidrs
   }
+
+  dynamic "encryption_config" {
+    for_each = [var.cluster_encryption_config]
+
+    content {
+      provider {
+        key_arn = encryption_config.value.provider_key_arn
+      }
+      resources = encryption_config.value.resources
+    }
+  }
+
+  tags = merge(
+    var.tags,
+    var.cluster_tags,
+  )
 }
+
+resource "aws_ec2_tag" "cluster_primary_security_group" {
+  for_each = { for k, v in merge(var.tags, var.cluster_tags) :
+    k => v if local.create && k != "Name" && var.create_cluster_primary_security_group_tags && v != null
+  }
+
+  resource_id = aws_eks_cluster.this[0].vpc_config[0].cluster_security_group_id
+  key         = each.key
+  value       = each.value
+}
+
+################################################################################
+# Cluster Security Group
+################################################################################
+
+locals {
+  cluster_sg_name = var.cluster_security_group_name != null ? var.cluster_security_group_name : "${local.cluster_name}-sg"
+
+  cluster_security_group_rules = { for k, v in {
+    ingress_nodes_443 = {
+      description                = "Node groups to cluster API"
+      protocol                   = "tcp"
+      from_port                  = 443
+      to_port                    = 443
+      type                       = "ingress"
+      source_node_security_group = true
+    }
+  } : k => v if local.create_node_sg }
+}
+
+resource "aws_security_group" "cluster" {
+  name        = local.cluster_sg_name
+  description = var.cluster_security_group_description
+  vpc_id      = var.vpc_id
+
+  tags = merge(
+    var.tags,
+    { "Name" = local.cluster_sg_name },
+    var.cluster_security_group_tags
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "cluster" {
+  for_each = { for k, v in var.cluster_security_group_additional_rules : k => v }
+
+  security_group_id = aws_security_group.cluster.id
+  protocol          = each.value.protocol
+  from_port         = each.value.from_port
+  to_port           = each.value.to_port
+  type              = each.value.type
+
+  description              = lookup(each.value, "description", null)
+  cidr_blocks              = lookup(each.value, "cidr_blocks", null)
+  ipv6_cidr_blocks         = lookup(each.value, "ipv6_cidr_blocks", null)
+  prefix_list_ids          = lookup(each.value, "prefix_list_ids", null)
+  self                     = lookup(each.value, "self", null)
+  source_security_group_id = try(each.value.source_node_security_group, false) ? local.node_security_group_id : lookup(each.value, "source_security_group_id", null)
+}
+
