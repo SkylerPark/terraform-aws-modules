03821aeadf57c7dbc1d9d0816f77a248456bc8db
SkylerPark <012tjdals012@gmail.com>
2023-11-27 23:04:58 +0900

    test: test

--- eks/providers-local.tf
+++ eks/providers-local.tf
@@ -1,9 +1,5 @@
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.id]
-  }
+  token                  = data.aws_eks_cluster.this.token
 }
